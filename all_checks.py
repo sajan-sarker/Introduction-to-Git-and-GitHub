d782cf00e185f09fe7a7efacf438e9052e30ec53
Sajan <sajansarker447@gmail.com>
2024-01-25 14:45:44 +0600

    add new function

--- all_checks.py
+++ all_checks.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
 import sys
 
 def check_reboot():
+	'''Return True if the computer has a pending reboot.'''
 	return os.path.exists("/run/reboot-required")
 
+def check_disk_full(disk):
+	'''Return True if there isn't enough disk space, False otherwise'''
+	du = shutil.disk_useage(disk)
+	free = du.free/ du.total * 100
+	return  free > 20
+def check_disk():
+	if check_disk_full('/'):
+		print("Error")
+	else:
+		print("Everything is OK!")
 def main():
 	if check_reboot():
 		print("Pending Reboot")
 		sys.exit(1)
 	print("Everything is OK!")
 	sys.exit(0)
+	check_disk()
+	
+	
 
 
 main()
