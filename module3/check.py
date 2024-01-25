7a81d5ad568413c517ec73fff6aeabf24bcd8763
Sajan <sajansarker447@gmail.com>
2024-01-25 20:30:08 +0600

    new file for checking system info

---
+++ module3/check.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import os
+import sys
+import shutil
+
+def check_reboot():
+    '''Return True if the computer has a pending reboot'''
+    return os.path.exists('/run/reboot-required')
+
+def check_disk_full(disk, min_gb, min_percent):
+    '''Returns True if there isn't enough disk space, False otherwise. '''
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / 2**30
+    # Calculate how many free gigabytes
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
+
+def main():
+    if check_reboot():
+        print("Pending Reboot. ")
+        sys.exit(1)
+    if check_disk_full(disk = '/', min_gb = 2, min_percent = 10):
+        print("Disk Full.")
+        sys.exit(1)
+
+    print("Everything Ok")
+    sys.exit(0)
+
+main()
